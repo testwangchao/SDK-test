b27084ea63eb45fa839bb10ea052b623dd0d18b3
Chaochao Wang <chaochao.wang@happyelements.com>
2019-12-18 15:06:08 +0800

    demo

--- driver.py
+++ driver.py
@@ -10,16 +10,19 @@ class InitDriver:
         self.device_name = device_name
 
     # Android
+    @property
     def init_driver(self):
-        log.info("init driver, device_name: %s") % self.device_name
+        log.info("init driver, device_name: %s" % self.device_name)
         try:
             mb = ut2.connect(self.device_name)
+
             # set default element wait timeout (20 seconds)
-            mb.wait_timeout(20)
+            mb.wait_timeout = 20
+
             # set delay 1s after each UI click and click
-            mb.click_post_delay(1)
+            mb.click_post_delay = 1
             log.info("connect %s success" % self.device_name)
             return mb
 
         except Exception as e:
-            log.info("init driver failed: %s") % e
+            log.info("init driver failed: %s" % e)
--- operate_sdk.py
+++ operate_sdk.py
@@ -1,22 +1,27 @@
 from driver import InitDriver
+import time
 
 
 class OperateSdk:
     def __init__(self, driver):
         self.driver = driver
-
+        
     def click(self, **kwargs):
-        a = lambda x:list(x.keys)
-        a(kwargs)[0] = kwargs.get(a(kwargs)[0])
+       self.driver(**kwargs).click()
 
 
 if __name__ == '__main__':
-    def func(**kwargs):
-        a = lambda x: list(x.keys())
-        a(kwargs)[0] = kwargs.get(a(kwargs)[0])
-        print(a(kwargs)[0])
-    func(v = 1)
+    driver = InitDriver(device_name="c6c1b81e")
+    d = driver.init_driver
+    sdk = OperateSdk(driver=d)
+    sdk.driver.app_start("com.sigmob.demo.android")
+    sdk.click(text="load dffa3806ae2")
+    # todo: check ad is ready by dclog
+    time.sleep(5)
+    sdk.click(text="play dffa3806ae2")
 
-    
-
-
+    if sdk.driver(description="立即下载").exists(timeout=100):
+        sdk.driver.xpath(
+            '//*[@resource-id="com.sigmob.demo.android:id/dokit_app_contentview_id"]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.RelativeLayout[2]').click()
+    else:
+        print("error")
