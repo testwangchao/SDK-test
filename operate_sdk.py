c9e8ef7ea347b9f273e0c661605ecf82ab241b72
Chaochao Wang <chaochao.wang@happyelements.com>
2019-12-18 15:28:43 +0800

    demo

--- operate_sdk.py
+++ operate_sdk.py
@@ -1,13 +1,19 @@
 from driver import InitDriver
 import time
+import re
 
 
 class OperateSdk:
     def __init__(self, driver):
         self.driver = driver
         
-    def click(self, **kwargs):
-       self.driver(**kwargs).click()
+    def click(self, element):
+        if re.findall("下载", element) or re.findall("详情", element):
+            self.driver(text=element).click()
+        elif re.findall("//", element):
+            self.driver.xpath(element).click()
+        elif re.findall("skip", element):
+            self.driver(description="skip").click()
 
 
 if __name__ == '__main__':
